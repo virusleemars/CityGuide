f4323e5088755f61c0c7dd8690e5d43865518329
viruslee <virusleeandroid@gmail.com>
2019-02-22 23:57:03 +0400

    first commit

---
+++ src/main/java/com/cityguide/Main.java
@@ -0,0 +1,14 @@
+package com.cityguide;
+
+import com.cityguide.core.City;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello");
+
+        City mCity = new City("Moskau", "Russia");
+        System.out.println(mCity);
+    }
+}
+
+
---
+++ src/main/java/com/cityguide/core/City.java
@@ -0,0 +1,35 @@
+package com.cityguide.core;
+
+public class City {
+    private String name;
+    private String country;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "name='" + name + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+
+    public City(String name, String country) {
+        setName(name);
+        setCountry(country);
+    }
+}
